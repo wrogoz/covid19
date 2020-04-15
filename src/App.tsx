85eb4805f285887cada9491e03b744f1b9d3220b
wrogoz <wrogoz@gmail.com>
2020-04-15 11:58:04 +0200

    basic styling including rwd

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import axios from 'axios';
 const App=()=> {
 
 const [activeCases,setActiveCases] = useState('loading...');
-const [inHospital,setInHospital] = useState('N/A');
+const [curedlCases,setCuredCases] = useState('loading...');
 const [criticalCases,setCriticalCases] = useState('loading...');
 const [totalDeaths,setTotalDeaths] = useState('loading...');
 const [totalCases,setTotalCases] = useState('loading...');
+const [country,setCountry]= useState('Poland')
   useEffect(() => {
     console.log('use effect status: ok')
     axios({
@@ -19,12 +20,13 @@ const [totalCases,setTotalCases] = useState('loading...');
       "x-rapidapi-host":"coronavirus-monitor.p.rapidapi.com",
       "x-rapidapi-key":"b60f46bea6msh7bbd55a04c4c2cdp1ebab7jsncb634d69134a"
       },"params":{
-      "country":"Poland"
+      "country":`${country}`
       }
       })
       .then((response)=>{
         console.log(response.data.latest_stat_by_country[0])
         setActiveCases(response.data.latest_stat_by_country[0].active_cases);
+        setCuredCases(response.data.latest_stat_by_country[0].total_recovered);
         setCriticalCases(response.data.latest_stat_by_country[0].serious_critical);
         setTotalDeaths(response.data.latest_stat_by_country[0].total_deaths);
         setTotalCases(response.data.latest_stat_by_country[0].total_cases);
@@ -36,11 +38,13 @@ const [totalCases,setTotalCases] = useState('loading...');
 
   return (
     <Container >
-      <div><p>Zakażeni: {activeCases}</p> </div>
-      <div><p>Hospitalizowani: {inHospital} </p> </div>
+
+      <div><p>Zakażeni: {activeCases} </p>  </div>
+      <div><p>Wyzdrowiało: {curedlCases} </p> </div>
       <div><p>Stan krytyczny: {criticalCases}</p> </div>
       <div><p>Zgony: {totalDeaths} </p></div>
-      <div><p>Łącznie: {totalCases}</p></div>
+      <div><p>Łącznie: {totalCases}
+      </p></div>
       <Signature>created by wrogoz</Signature>
     </Container>
   );
@@ -53,9 +57,10 @@ const [totalCases,setTotalCases] = useState('loading...');
 
     display:flex;
     flex-direction:column;
-
+    align-items:center;
+    background-color:#36393B;
     div{
-
+      max-width:800px;
       display:flex;
       align-items:center;
       justify-content:center;
@@ -70,12 +75,13 @@ const [totalCases,setTotalCases] = useState('loading...');
       }
 
       p{
+        z-index:1000;
         color:#EEE6AB;
-        font-size: 1.5rem;
+        font-size: 1.8rem;
         text-transform:uppercase;
         padding:0;
         margin:0;
-      }
+
     }
   `
   const Signature = styled.p`
@@ -85,5 +91,15 @@ const [totalCases,setTotalCases] = useState('loading...');
     transform: rotate(-90deg);
     color: #C5BC8E;
     font-size: 0.8rem;
+  `
+  const Country = styled.p`
+  z-index:200;
+  position: absolute;
+  top: 39%;
+  left: -15%;
+  transform: rotate(-54deg);
+    z-index:200;
+  font-size: 9rem;
+  color:#36393B66;
   `
 export default App;
